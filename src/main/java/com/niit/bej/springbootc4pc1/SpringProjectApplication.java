2d46d40f2febe218460dc401844a92366028c616
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-26 22:57:31 +0530

    Initial commit

---
+++ src/main/java/com/niit/bej/springbootc4pc1/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.springbootc4pc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/bej/springbootc4pc1/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.springbootc4pc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
